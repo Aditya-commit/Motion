b2d807f9a6def051774f2cca34b146c2eb6edc47
Aditya Tomar <adityatomar80050@gmail.com>
2026-05-04 15:45:42 +0530

    Commit

--- src/app/components/layoutAnimation.tsx
+++ src/app/components/layoutAnimation.tsx
@@ -1,5 +1,7 @@
-import { AnimatePresence, motion } from "motion/react";
 import { useState } from "react";
+import { motion } from "motion/react";
+
+// HERE WE DO NOT NEED OT USE ANIMATEPRESENCE SINCE THERE IS NO EXIT ANIMATION TO IT
 
 const LayoutAnimation = () => {
 
@@ -9,15 +11,15 @@ const LayoutAnimation = () => {
 
     return(
         <motion.div className={`grid ${expand ? 'grid-cols-[60%_40%]' : 'grid-cols-[100%_0%]'} h-dvh`} layout>
-            <motion.div layout='position' className="h-full bg-black">
+            <motion.div layout='size' className="h-full bg-black">
                 {expand
                 ?
-                <button className='bg-white rounded-full px-6 py-2 text-black' style={{transform : 'none'}} onClick={toggleExpand}>Contract</button>
+                <motion.button layout layoutAnchor={{ x : 0.5 , y : 0.5}} className='bg-white rounded-full px-6 py-2 text-black' style={{transform : 'none'}} onClick={toggleExpand}>Contract</motion.button>
                 :
-                <button className='bg-white rounded-full px-6 py-2 text-black' style={{transform : 'none'}} onClick={toggleExpand}>Expand</button>
+                <motion.button layout layoutAnchor={{ x : 0.5 , y : 0.5}} className='bg-white rounded-full px-6 py-2 text-black' style={{transform : 'none'}} onClick={toggleExpand}>Expand</motion.button>
                 }
             </motion.div>
-            <motion.div layout='position' className='h-full bg-red-500'></motion.div>
+            <motion.div layout='size' className='h-full bg-red-500'></motion.div>
         </motion.div>
     )
 }
